a6a09bbaca28a2e5cf30c2606b53e91576eeebda
Michael Abbott <michael.abbott@diamond.ac.uk>
2023-07-17 13:54:02 +0100

    Add reset synchronisation support to sync_bit

--- vhd/util/sync_bit.vhd
+++ vhd/util/sync_bit.vhd
@@ -13,7 +13,9 @@ entity sync_bit is
     port (
         clk_i : in std_ulogic;
         bit_i : in std_ulogic;
-        bit_o : out std_ulogic
+        bit_o : out std_ulogic;
+        -- If desired asynchronous reset can be synchronised to clk_i
+        reset_i : in std_ulogic := '0'
     );
 end;
 
@@ -34,8 +36,11 @@ architecture arch of sync_bit is
     attribute false_path_to of bit_in : signal is "TRUE";
 
 begin
-    process (clk_i) begin
-        if rising_edge(clk_i) then
+    process (clk_i, reset_i) begin
+        if reset_i then
+            bit_in <= INITIAL;
+            bit_out <= INITIAL;
+        elsif rising_edge(clk_i) then
             bit_in <= bit_i;
             bit_out <= bit_in;
         end if;
