4e6114470819fce71e8e62155ee75251e0937b0b
Michael Abbott <michael.abbott@diamond.ac.uk>
2023-07-24 10:55:29 +0100

    Update defaults for ibufds_array

--- vhd/iodefs/ibufds_array.vhd
+++ vhd/iodefs/ibufds_array.vhd
@@ -11,7 +11,9 @@ use unisim.vcomponents.all;
 
 entity ibufds_array is
     generic (
-        COUNT : natural := 1
+        COUNT : natural := 1;
+        IOSTANDARD : string := "DEFAULT";
+        DIFF_TERM : boolean := true
     );
     port (
         p_i : in  std_ulogic_vector(COUNT-1 downto 0);
@@ -24,8 +26,8 @@ architecture arch of ibufds_array is
 begin
     ibufds_array : for i in 0 to COUNT-1 generate
         ibufds_inst: IBUFDS generic map (
-            IOSTANDARD => "LVDS",
-            DIFF_TERM => true
+            IOSTANDARD => IOSTANDARD,
+            DIFF_TERM => DIFF_TERM
         ) port map (
             I  => p_i(i),
             IB => n_i(i),
