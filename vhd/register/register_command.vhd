dd24df4328294b03e809e6b2c739eedc79956f79
Michael Abbott <michael.abbott@diamond.ac.uk>
2023-09-05 11:49:47 +0100

    Change register_command to add support for bit acknowledgement
    Also remove built-in delay of output control bits, this is probably not
    required and better done explicitly when needed.

--- vhd/register/register_command.vhd
+++ vhd/register/register_command.vhd
@@ -17,34 +17,34 @@ entity register_command is
         write_ack_o : out std_ulogic := '0';
 
         -- Output strobed bits
-        strobed_bits_o : out reg_data_t
+        strobed_bits_o : out reg_data_t;
+        -- Optional acknowledge per strobed bit
+        strobed_ack_i : in reg_data_t := (others => '1')
     );
 end;
 
 architecture arch of register_command is
     signal strobed_bits : reg_data_t := (others => '0');
+    signal busy_bits : reg_data_t := (others => '0');
+    signal need_ack_bits : reg_data_t;
+    signal unacked_bits : reg_data_t;
+    signal ack_ready : std_ulogic;
 
 begin
+    need_ack_bits <= write_data_i when write_strobe_i else busy_bits;
+    unacked_bits <= need_ack_bits and not strobed_ack_i;
+    ack_ready <= not vector_or(unacked_bits);
     process (clk_i) begin
         if rising_edge(clk_i) then
-            if write_strobe_i = '1' then
-                strobed_bits <= write_data_i;
+            strobed_bits <= write_strobe_i and write_data_i;
+            busy_bits <= unacked_bits;
+
+            if write_strobe_i then
+                write_ack_o <= ack_ready;
             else
-                strobed_bits <= (others => '0');
+                write_ack_o <= vector_or(busy_bits) and ack_ready;
             end if;
-            -- Delay the ack so that it's synchronous with our delayed strobe.
-            -- This will avoid problems if the next register write depends on
-            -- side effects of the strobe.
-            write_ack_o <= write_strobe_i;
         end if;
     end process;
-
-    -- Registers to ease timing for distribution of strobes
-    delay_strobe : entity work.dlyreg generic map (
-        WIDTH => strobed_bits_o'LENGTH
-    ) port map (
-        clk_i => clk_i,
-        data_i => strobed_bits,
-        data_o => strobed_bits_o
-    );
+    strobed_bits_o <= strobed_bits;
 end;
