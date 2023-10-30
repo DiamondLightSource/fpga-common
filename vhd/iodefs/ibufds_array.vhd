ca3d83a63399fa1e9cd87808c816f36106f4a307
Michael Abbott <michael.abbott@diamond.ac.uk>
2023-10-30 08:08:01 +0000

    Add pin swap support to ibufds_array and obufds_array

--- vhd/iodefs/ibufds_array.vhd
+++ vhd/iodefs/ibufds_array.vhd
@@ -13,7 +13,8 @@ entity ibufds_array is
     generic (
         COUNT : natural := 1;
         IOSTANDARD : string := "DEFAULT";
-        DIFF_TERM : boolean := true
+        DIFF_TERM : boolean := true;
+        PIN_SWAPS : std_ulogic_vector(COUNT-1 downto 0) := (others => '0')
     );
     port (
         p_i : in  std_ulogic_vector(COUNT-1 downto 0);
@@ -25,12 +26,20 @@ end;
 architecture arch of ibufds_array is
 begin
     ibufds_array : for i in 0 to COUNT-1 generate
+        signal pin : std_ulogic;
+        signal pin_b : std_ulogic;
+    begin
+        -- If pin swapping is required on this input assign n_i to I, otherwise
+        -- assign p_i, and similarly for IB.
+        pin   <= n_i(i) when PIN_SWAPS(i) else p_i(i);
+        pin_b <= p_i(i) when PIN_SWAPS(i) else n_i(i);
+
         ibufds_inst: IBUFDS generic map (
             IOSTANDARD => IOSTANDARD,
             DIFF_TERM => DIFF_TERM
         ) port map (
-            I  => p_i(i),
-            IB => n_i(i),
+            I  => pin,
+            IB => pin_b,
             O  => o_o(i)
         );
     end generate;
--- vhd/iodefs/obufds_array.vhd
+++ vhd/iodefs/obufds_array.vhd
@@ -12,7 +12,8 @@ use unisim.vcomponents.all;
 entity obufds_array is
     generic (
         COUNT : natural := 1;
-        IOSTANDARD : string := "DEFAULT"
+        IOSTANDARD : string := "DEFAULT";
+        PIN_SWAPS : std_ulogic_vector(COUNT-1 downto 0) := (others => '0')
     );
     port (
         i_i : in  std_ulogic_vector(COUNT-1 downto 0);
@@ -24,12 +25,20 @@ end;
 architecture arch of obufds_array is
 begin
     obufds_array : for i in 0 to COUNT-1 generate
+        signal pin : std_ulogic;
+        signal pin_b : std_ulogic;
+    begin
         obufds_inst: OBUFDS generic map (
             IOSTANDARD => IOSTANDARD
         ) port map (
             I  => i_i(i),
-            O  => p_o(i),
-            OB => n_o(i)
+            O  => pin,
+            OB => pin_b
         );
+
+        -- If pin swapping is required on this input assign O to n_o, otherwise
+        -- assign to n_p, and similarly for OB.
+        p_o(i) <= pin_b when PIN_SWAPS(i) else pin;
+        n_o(i) <= pin when PIN_SWAPS(i) else pin_b;
     end generate;
 end;
