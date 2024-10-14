27122384e18617bb1f19bb32825da4f6e10e7e3d
Michael Abbott <michael.abbott@diamond.ac.uk>
2024-10-14 08:43:37 +0100

    Rewrite simple fifo
    This is hopefully a clear style, takes advantage of the extra address bit
    trick to distinguish full from empty, and has simpler management of the
    output buffer.

--- vhd/util/fifo.vhd
+++ vhd/util/fifo.vhd
@@ -22,11 +22,11 @@ entity fifo is
 
         -- Write interface
         write_valid_i : in std_ulogic;
-        write_ready_o : out std_ulogic;
+        write_ready_o : out std_ulogic := '0';
         write_data_i : in std_ulogic_vector(DATA_WIDTH-1 downto 0);
 
         -- Read interface
-        read_valid_o : out std_ulogic;
+        read_valid_o : out std_ulogic := '0';
         read_ready_i : in std_ulogic;
         read_data_o : out std_ulogic_vector(DATA_WIDTH-1 downto 0);
 
@@ -38,111 +38,100 @@ end;
 
 architecture arch of fifo is
     subtype DATA_RANGE is natural range DATA_WIDTH-1 downto 0;
+    subtype ADDRESS_RANGE is natural range 0 to 2**FIFO_BITS-1;
+    subtype ADDRESS_RANGE_BITS is natural range FIFO_BITS downto 0;
 
-    -- The FIFO is structured into four parts: INPUT, OUTPUT, STORE, STATE.
-
-    -- STORE where the fifo data is stored.
-    signal in_ptr  : unsigned(FIFO_BITS-1 downto 0) := (others => '0');
-    signal out_ptr : unsigned(FIFO_BITS-1 downto 0) := (others => '0');
-    signal fifo : vector_array(0 to 2**FIFO_BITS-1)(DATA_RANGE);
-
+    signal fifo : vector_array(ADDRESS_RANGE)(DATA_RANGE);
     attribute RAM_STYLE : string;
     attribute RAM_STYLE of fifo : signal is MEM_STYLE;
 
-    -- STATE: we detect both "full" and "nearly full" conditions, and so it
-    -- turns out that the logic is simpler if we use a full flip-flop to
-    -- distinguish between empty and full states.
-    signal full : boolean := false;
-    signal nearly_full : boolean;
-    signal empty : boolean;
+    -- This is just computing a mask with only the top bit set to be used for
+    -- detecting the FIFO full condition
+    function COMPARE_MASK return unsigned
+    is
+        variable result : unsigned(ADDRESS_RANGE_BITS) := (others => '0');
+    begin
+        result(FIFO_BITS) := '1';
+        return result;
+    end;
 
-    -- INPUT: we register the incoming data and the write ready state.
-    signal write_data_valid : boolean := false;
-    signal write_ready : boolean := true;
-    signal write_data : std_ulogic_vector(DATA_RANGE);
-    signal do_write : boolean;
-    signal write_enable : boolean;
-
-    -- OUTPUT: register read valid state
-    signal read_valid : boolean := false;
-    signal do_read : boolean;
-    signal read_data : std_ulogic_vector(DATA_RANGE) := (others => '0');
+    signal write_pointer : unsigned(ADDRESS_RANGE_BITS) := (others => '0');
+    signal read_pointer : unsigned(ADDRESS_RANGE_BITS) := (others => '0');
+    -- The read valid state is separated from read_valid_o to improve data flow
+    signal read_valid : std_ulogic := '0';
 
 begin
-    assert FIFO_BITS > 0
-        report "Cannot use FIFO with empty memory"
-        severity failure;
+    process (clk_i)
+        variable read_enable : std_ulogic;
+        variable next_write_pointer : unsigned(ADDRESS_RANGE_BITS);
+        variable next_read_pointer : unsigned(ADDRESS_RANGE_BITS);
+        variable write_address : ADDRESS_RANGE;
+        variable read_address : ADDRESS_RANGE;
 
-    -- STATE
-    nearly_full <= in_ptr + 1 = out_ptr;
-    empty <= in_ptr = out_ptr and not full;
-
-    -- INPUT
-    write_enable <= write_ready and write_valid_i = '1';
-    do_write <= write_data_valid and not full;
-
-    -- OUTPUT
-    do_read <= not empty and (read_ready_i = '1' or not read_valid);
-
-
-    process (clk_i) begin
+    begin
         if rising_edge(clk_i) then
-            -- STORE
-            if reset_fifo_i = '1' then
-                in_ptr <= (others => '0');
-            elsif do_write then
-                fifo(to_integer(in_ptr)) <= write_data;
-                in_ptr <= in_ptr + 1;
-            end if;
-
-            if reset_fifo_i = '1' then
-                out_ptr <= (others => '0');
-            elsif do_read then
-                read_data <= fifo(to_integer(out_ptr));
-                out_ptr <= out_ptr + 1;
-            end if;
-
-            -- STATE
-            if reset_fifo_i = '1' then
-                full <= false;
-            elsif nearly_full and do_write and not do_read then
-                full <= true;
-            elsif do_read then
-                full <= false;
-            end if;
-
-            -- INPUT
-            if reset_fifo_i = '1' then
-                write_ready <= false;
-                write_data_valid <= false;
+            if reset_fifo_i then
+                write_pointer <= (others => '0');
+                read_pointer <= (others => '0');
+                write_ready_o <= '0';
+                read_valid <= '0';
+                read_enable := '0';
             else
-                write_ready <= not (full or nearly_full);
-                if write_enable then
-                    write_data_valid <= true;
-                    write_data <= write_data_i;
-                elsif do_write then
-                    write_data_valid <= false;
+                -- Advance write pointer if writing
+                if write_valid_i and write_ready_o then
+                    next_write_pointer := write_pointer + 1;
+                else
+                    next_write_pointer := write_pointer;
                 end if;
+
+                -- Advance read pointer if reading.  We keep read_data_o valid
+                -- at all times when possible
+                read_enable :=
+                    read_valid and (read_ready_i or not read_valid_o);
+                if read_enable then
+                    next_read_pointer := read_pointer + 1;
+                else
+                    next_read_pointer := read_pointer;
+                end if;
+
+                write_pointer <= next_write_pointer;
+                read_pointer <= next_read_pointer;
+
+                -- Compute full and empty conditions.  Empty is simply equality
+                -- of pointers, full is when the write pointer is exactly one
+                -- FIFO depth ahead of the read pointer.  This computation can
+                -- safely be done on the next_ pointers, which gives a small
+                -- flow optimisation.
+                write_ready_o <= to_std_ulogic(
+                    next_write_pointer /= (next_read_pointer xor COMPARE_MASK));
+                read_valid <= to_std_ulogic(
+                    next_write_pointer /= next_read_pointer);
             end if;
 
-            -- OUTPUT
-            if reset_fifo_i = '1' then
-                read_valid <= false;
-            elsif do_read then
-                read_valid <= true;
-            elsif read_ready_i = '1' and not do_read then
-                read_valid <= false;
+            -- Throw away the top bit of the read/write pointers for access, the
+            -- top bit is only used as a cycle counter to distinguish full and
+            -- empty states
+            write_address := to_integer(write_pointer(FIFO_BITS-1 downto 0));
+            read_address := to_integer(read_pointer(FIFO_BITS-1 downto 0));
+
+            -- Write
+            if write_valid_i and write_ready_o then
+                fifo(write_address) <= write_data_i;
             end if;
 
-            -- Readout of FIFO depth
-            -- Calculating the depth is surprisingly tricky, as we have to
-            -- account for an extra value in the output buffer
+            -- Read
+            if reset_fifo_i then
+                read_valid_o <= '0';
+            elsif read_enable then
+                read_data_o <= fifo(read_address);
+                read_valid_o <= '1';
+            elsif read_ready_i then
+                read_valid_o <= '0';
+            end if;
+
+            -- Compute number of points in FIFO, also counting the output buffer
             fifo_depth_o <=
-                (to_std_ulogic(full) & (in_ptr - out_ptr)) + read_valid_o;
+                next_write_pointer - next_read_pointer + read_valid_o;
         end if;
     end process;
-
-    write_ready_o <= to_std_ulogic(write_ready);
-    read_valid_o  <= to_std_ulogic(read_valid);
-    read_data_o <= read_data;
 end;
